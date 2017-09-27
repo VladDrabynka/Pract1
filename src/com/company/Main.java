7897c7d1c1cff6eb2971a741f96e839ec04b1e6f
VladDrabynka <vlad.un1que.96@gmail.com>
2017-09-27 23:02:27 +0300

    Sort method (bubble) added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        sort(args);
         print(args);
     }
 
@@ -10,6 +11,22 @@ public class Main {
     {
         for (String arg: args) {
             System.out.println(arg);
+        }
+    }
+
+    public static void sort(String[] args)
+    {
+        for(int i=0; i<args.length; i++)
+        {
+            for(int j=0; j<args.length - i - 1; j++)
+            {
+                if(args[j].compareTo(args[j+1])>0)
+                {
+                    String temp = args[j];
+                    args[j]=args[j+1];
+                    args[j+1]=temp;
+                }
+            }
         }
     }
 }
